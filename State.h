172cbcda2539c670a6f94e99fb4e85d3b82330a3
Glebos <gleb.trufanov2013@yandex.ru>
2020-04-10 04:14:57 +0300

    StringState and RealNumState

--- State.h
+++ State.h
@@ -1,19 +1,12 @@
-//
-// Created by artem on 10.04.2020.
-//
-
 #ifndef RUSTCOMPILER_STATE_H
 #define RUSTCOMPILER_STATE_H
 
-#include <vector>
 #include <iostream>
-#include <algorithm>
 
 class State {
 public:
-    virtual bool Contains(char sym) = 0;
-    virtual bool CanTransitTo(char sym) = 0;
+    virtual bool Contains(char sym) const = 0;
+    virtual bool CanTransitTo(char sym) const = 0;
 };
-
 
 #endif //RUSTCOMPILER_STATE_H
