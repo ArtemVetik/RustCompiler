d8ad02145dd743587ff250c1f2a24e6a9be07aaa
Gleb <gleb.trufanov2013@yandex.ru>
2020-04-13 16:08:22 +0300

    Token id

--- Token.cpp
+++ Token.cpp
@@ -1,12 +1,16 @@
 #include "Token.h"
 
-Token::Token(const TokenType &type, std::string value) : _type(type), _value(std::move(value)) {
+Token::Token(const TokenType &type, std::string value, const int id) : _type(type), _value(std::move(value)), _id(id) {
 
 }
 
-std::string Token::Value(){
+std::string Token::GetValue() const {
     return _value;
 }
-TokenType Token::Type(){
-    return  _type;
+TokenType Token::GetType() const {
+    return _type;
+}
+
+void Token::PrintToken() const {
+    std::cout << AAAPrintTokenType(_type) << std::string("-", 10) << _value << std::string("-", 10) << _id << std::endl;
 }
